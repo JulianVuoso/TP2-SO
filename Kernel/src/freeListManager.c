4b022a58050cb7f8d10fcf6ba546708b6a5bddf5
mbrula <mbrula@itba.edu.ar>
2019-09-27 17:23:20 -0300

    free changes
    Signed-off-by: mbrula <mbrula@itba.edu.ar>

--- Kernel/src/freeListManager.c
+++ Kernel/src/freeListManager.c
@@ -66,22 +66,29 @@ void * malloc(uint64_t bytes) {
 
 void free(void * ptr) {
     /* SEARCH of the ptr on used list */
-    node * iterator = memory.usedList;
-
     // creates a pointer to the real start of the block
     uint8_t * pointer = (uint8_t *)ptr - ALIGNEMENT(node);
+
+    node * iterator = memory.usedList;
+    node * prev = iterator;
     
     // search for the pointer
-    while (iterator != 0 && iterator->address != pointer)
+    while (iterator != 0 && iterator->address != pointer) {
+        prev = iterator;
         iterator = iterator->next;
-
+    }
+        
     // if not found
     if (iterator == 0) return;
     node * blockToFree = iterator;
 
+    // updates used list
+    if (memory.usedList == blockToFree) memory.usedList = blockToFree->next;
+    else prev->next = blockToFree->next;
+
     /* SEARCH found block on free list */
     iterator = memory.freeList;
-    node * prev = iterator;
+    prev = iterator;
 
     // searches the correct position to insert
     while (iterator != 0 && iterator->address < blockToFree->address) {
@@ -89,13 +96,23 @@ void free(void * ptr) {
         iterator = iterator->next;
     }
     
-    // inserts after the node prev
-    blockToFree->next = prev->next;
-    blockToFree->prev = prev;
-    prev->next = blockToFree;
+    // inserts between node prev and iterator
+    
+    // if its the first place
+    if (prev == iterator) {
+        memory.freeList = blockToFree;
+        blockToFree->prev = 0;
+        blockToFree->next = iterator;
+    } else {
+        blockToFree->prev = prev;
+        blockToFree->next = prev->next;
+        prev->next = blockToFree;
+        if (iterator != 0) iterator->prev = blockToFree; // if not last
+    } 
 
     // updates header values
-
+    memory.occupied -= blockToFree->size;
+    memory.free += blockToFree->size;
 
     // checks for contiguous free blocks (next or prev) and joins them
     check_contiguity(blockToFree);
