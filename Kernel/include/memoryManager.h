2a0abaec5eea81c513c96a5b92a0efcc64248111
mbrula <mbrula@itba.edu.ar>
2019-09-27 10:53:49 -0300

    added memory manager header (interface)
    Signed-off-by: mbrula <mbrula@itba.edu.ar>

---
+++ Kernel/include/memoryManager.h
@@ -0,0 +1,18 @@
+/***************************************************
+    MemoryManager.h
+****************************************************/
+
+#ifndef _memmanager_
+#define _memmanager_
+
+#include <stdint.h>
+
+/* Memory Manager builder */
+int createManager(uint8_t adress, uint64_t pageSize, uint64_t maxPages);
+
+/* Memory Manager user functions */
+void * malloc(uint64_t size);
+void free(void * ptr);
+void status(uint64_t * total, uint64_t * ocupied, uint64_t * free);
+
+#endif
