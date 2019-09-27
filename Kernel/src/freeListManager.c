4745fc585cbc248fa8ad321d9a3290839234dbd7
julian <jtallar@itba.edu.ar>
2019-09-27 17:50:33 -0300

    Added Malloc for first fit free list

--- Kernel/src/freeListManager.c
+++ Kernel/src/freeListManager.c
@@ -5,23 +5,10 @@
 
 static header memory;
 
-typedef struct mem_header {
-    uint8_t * freeList;
-    uint8_t * usedList;
- 
-    //  measured in pages
-    uint64_t total;
-    uint64_t ocupied;
-    uint64_t free;
-
-    // page size in bytes
-    uint64_t pageSize;
-} header;
-
 typedef struct mem_node {
     // pointers to the next and previous memory block nodes
-    uint8_t * next;
-    uint8_t * prev;
+    struct mem_node * next;
+    struct mem_node * prev;
 
     // pointer to start of the block (node)
     uint8_t * address;
@@ -30,38 +17,81 @@ typedef struct mem_node {
     uint64_t size;
 } node;
 
+typedef struct mem_header {
+    node * freeList;
+    node * usedList;
+ 
+    //  measured in pages
+    uint64_t occupied;
+    uint64_t free;
+
+    // page size in bytes
+    uint64_t pageSize;
+} header;
+
 int create_manager(uint8_t * address, uint64_t pageSize, uint64_t maxPages) {
     // check for errors
     if (address + pageSize * maxPages > MAX_DIR) return -1;   
     
     // initialize list header
-    memory.total = memory.free = maxPages;
-    memory.freeList = address;
+    memory.total = maxPages;
+    memory.free = maxPages;
+    memory.occupied = 0;
+    memory.freeList = (node *) address;
+    memory.usedList = 0;
     memory.pageSize = pageSize;
 
     // create first block of maxPages pages
-    node first = {0, 0, address, maxPages};
+    node first;
+    first.next = 0;
+    first.prev = 0;
+    first.address = address;
+    first.size = maxPages;
+
     memcpy(address, &first, sizeof(node));
 
     return 0;
 }
 
 void * malloc(uint64_t bytes) {
-    // convertir a nimero de paginas sumandole aLiGNEMnte node
+    // Obtener numero de paginas solicitadas, tomando en cuenta el espacio del nodo
+    uint64_t pageCount = (bytes + ALIGNEMENT(node)) / memory.pageSize + 1;
+    if (pageCount > memory.free) return 0; // No tengo paginas disponibles
 
-    // recorro nodos hasta encontrar uno de tamano >= a lo que quiero
+    // Recorro nodos de freeList hasta encontrar uno con capacidad suficiente
+    node * found = memory.freeList;
+    while (found != 0 && found->size < pageCount) {
+        found = found->next;
+    }
+    if (found == 0) return 0; // No tengo espacio contiguo suficiente
 
-    // creo nodo nueo que apunte comienzo siguiente bloque si me sobra espacio
+    // Chequeo si me sobran paginas
+    if (found->size > pageCount) {
+        // Creo nodo nuevo que apunte al comienzo del siguiente bloque, con las paginas restantes y lo conecto
+        node newNode;
+        newNode.prev = found->prev;
+        newNode.next = found->next;
+        newNode.address = (node *) (found->address + pageCount * memory.pageSize);
+        newNode.size = found->size - pageCount;
+        memcpy(newNode.address, &newNode, sizeof(node));
+        if (found->prev == 0)
+            memory.freeList = (node *) newNode.address;
+        else 
+            found->prev->next = newNode.address;
+    }
 
-    // lo conecto al anterior si me sobrea espacio (en free list)
+    // Modifico las propiedades del nodo extraido y lo agrego a usedList
+    found->size = pageCount;
+    found->prev = 0;
+    found->next = memory.usedList;
+    memory.usedList = (node *) found->address;
+    
+    // Modifico las propiedades de la memoria
+    memory.occupied += pageCount;
+    memory.free -= pageCount;
 
-    // cambio nodo del bloque a reservar
-
-    // concecto nodo en ocupied
-
-    // cambio variables generales
-
-
+    // Devuelvo la direccion del nodo extraido desfasada ALIGNEMENT(node)
+    return found->address + ALIGNEMENT(node);
 }
 
 void free(void * ptr) {
@@ -72,7 +102,7 @@ void free(void * ptr) {
 
     // recorremos y buscamos bloque con address ptr
 
-    // recorremos freelist desde principio y comparamos posiciones de memoria
+    // recorremos freelist desde principio y comparamos posiciones de memory
 
     // inserto ahi en el medio
 
@@ -80,7 +110,7 @@ void free(void * ptr) {
 }
 
 void status(uint64_t * total, uint64_t * occupied, uint64_t * free) {
-    *total = memory.total * memory.pageSize;
+    *total = (memory.free + memory.occupied) * memory.pageSize;
     *occupied = memory.occupied * memory.pageSize;
     *free = memory.free * memory.pageSize;
 }
